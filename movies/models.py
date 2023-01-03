81e5004af316c6378ddbb3d8655eacd9ad2ecc98
mahavirbha <100768509+mahavir-amoga@users.noreply.github.com>
2023-01-03 23:37:03 +0530

    image field adde

--- movies/models.py
+++ movies/models.py
@@ -10,3 +10,6 @@ class Moviedata(models.Model):
     duration = models.FloatField()
     rating = models.FloatField()
     typ = models.CharField(max_length=200, default='action')
+    image = models.ImageField(upload_to='Images/', default="Images/None/noimg.jpg")
+
+
