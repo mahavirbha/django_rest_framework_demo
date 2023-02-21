0a349c4f1053e69b53222e7f59e7b5180bc672ce
Mahavirsinh Chauhan <100768509+mahavir-amoga@users.noreply.github.com>
2023-02-21 09:42:33 +0530

    db changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db2',
+        'USER': 'db2',
+        'PASSWORD': '1234567890',
+        'HOST': 'db2.cmihumfay1pk.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
